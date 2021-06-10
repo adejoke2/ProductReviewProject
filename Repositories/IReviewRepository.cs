2ba6a4beefd382f2957f4e6a27b8c792cacb961c
Olowonmihabeebah <olowonmiadejoke111@gmail.com>
2021-06-10 09:53:09 +0100

    initial commit

---
+++ Repositories/IReviewRepository.cs
@@ -0,0 +1,24 @@
+﻿using ProductReviewSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductReviewSystem.Repositories
+{
+    public interface IReviewRepository
+    {
+        public Review AddReview(Review review);
+
+        public Review FindById(int id);
+
+        public List<Review> FindByProductId(int productId);
+
+        public Review UpdateReview(Review review);
+
+        public void Delete(int id);
+
+        public List<Review> GetReviews();
+        
+    }
+}
---
+++ Services/IProductService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using ProductReviewSystem.Models;
+using ProductReviewSystem.Models.ViewModels;
+using System.Collections.Generic;
+
+namespace ProductReviewSystem.Services
+{
+    public interface IProductService
+    {
+        public Product AddProduct(CreateProductViewModel model);
+     
+        public void Delete(int id);
+
+        public Product FindById(int id);
+
+        public List<ProductViewModel> GetProducts();
+
+        public List<Product> GetReviewProducts(int userId);
+
+        public Product UpdateProduct(UpdateProductViewModel model);
+
+        public IEnumerable<SelectListItem> GetProductList();
+
+        public IEnumerable<SelectListItem> GetReviewProductList(int userId);
+    }
+}
